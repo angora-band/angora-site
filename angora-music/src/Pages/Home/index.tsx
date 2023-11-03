07c2cb1b405bcf829f0dce78b57dacd7d65addd6
Deuce Black <deuce2black@gmail.com>
2023-11-03 17:48:04 -0500

    initial commit - added react + router + tailwind

---
+++ angora-music/src/Pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {};
+
+const Home = (props: Props) => {
+    return (
+        <p className='text-red-700'>
+            Hello world
+        </p>
+    );
+};
+
+export default Home;
